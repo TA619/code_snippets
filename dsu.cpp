e2cbfe63b83b9d63b292aed26330fdeb41e3c19d
TA619 <triyanshag619@gmail.com>
2026-03-24 22:18:59 +0530

    Add DSU class with union-find functionality
    Implement disjoint set union (DSU) class with union and find methods.

--- dsu.cpp
+++ dsu.cpp
@@ -1,1 +1,27 @@
+class dsu{
+    vector<int> parent, rank;
+public: 
+    dsu(n){
+        parent.resize(n);
+        rank.resize(n, 1);
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }
+    int ulp(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = ulp(parent[x]);
+    }
+     void join(int u, int v){
+        int pu = ulp(u);
+        int pv = ulp(v);
 
+        if(pu == pv) return;
+        
+        if(size[pu] > size[pv]){
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        } else {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+    }
+};
