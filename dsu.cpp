673678c522458ba83725c1295752241f25ddb0c9
TA619 <triyanshag619@gmail.com>
2026-03-24 22:21:03 +0530

    DSU code snippet

--- dsu.cpp
+++ dsu.cpp
@@ -1,16 +1,19 @@
 class dsu{
-    vector<int> parent, rank;
+    vector<int> parent, size;
+
 public: 
-    dsu(n){
+    dsu(int n){
         parent.resize(n);
-        rank.resize(n, 1);
+        size.resize(n, 1);
         for(int i = 0; i < n; i++) parent[i] = i;
     }
+
     int ulp(int x){
         if(parent[x] == x) return x;
         return parent[x] = ulp(parent[x]);
     }
-     void join(int u, int v){
+
+    void join(int u, int v){
         int pu = ulp(u);
         int pv = ulp(v);
 
